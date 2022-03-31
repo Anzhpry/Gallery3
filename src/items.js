03d299f26a750c2d4a1bf129f5323bc8766333cf
Anzhelika <anzh201419@yandex.ua>
2022-03-31 17:26:52 +0300

    Домашнее задание 3

---
+++ src/items.js
@@ -0,0 +1,42 @@
+import image1 from '../media/1.jpg'
+import image2 from '../media/2.jpg'
+import image3 from '../media/3.jpg'
+import image4 from '../media/4.gif'
+
+import audio1 from '../media/1.mp3'
+
+import video1 from '../media/1.mp4'
+
+export default [
+    {
+        resource: image1,
+        type: 'image',
+        title: 'Мельница у реки'
+    },
+    {
+        resource: image2,
+        type: 'image',
+        title: 'Горы осенью'
+    },
+    {
+        resource: image3,
+        type: 'image',
+        title: 'Деревья летом'
+    },
+    {
+        resource: image4,
+        type: 'image',
+        title: 'Анимированный котик'
+    },
+    {
+        resource: audio1,
+        type: 'audio',
+        title: 'Птички летом'
+
+    },
+    {
+        resource: video1,
+        type: 'video',
+        title: 'Морской прибой'
+    }
+]
